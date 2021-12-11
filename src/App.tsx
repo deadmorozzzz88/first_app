973705e357ffc7ede2126e240e9ad33894ae4204
Nikita Popov <n.popov88@mail.ru>
2021-12-11 13:11:41 +0300

    props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import ProductCounter from "./ProductCounter";
 
 const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <ProductCounter product={{ productName: "Orange", count: 6 }} />
+      <ProductCounter product={{ productName: "Apple", count: 1 }} />
+      <ProductCounter product={{ productName: "Carrot", count: 9 }} />
+      <ProductCounter product={{ productName: "Pineapple", count: 4 }} />
+      <ProductCounter product={{ productName: "Tomato", count: 7 }} />
     </div>
   );
 };
